568dd6c03a135f42ed29928a817981ef7a00a1cb
mooven <mooven@meyicloud.com>
2025-04-02 17:59:39 +0530

    ee

--- pipeLine/shell.sh
+++ pipeLine/shell.sh
@@ -1,20 +1,34 @@
-#!/bin/bash
+#! /bin/bash
 
 # Accept the environment argument
 arg=$1
 echo $arg
 
 # Source the environment variables from the given file
-source ./$arg.sh
+source ./env/$arg.sh
 
-echo "Environment Variable successfully Imported"
+echo "Environment Variables Successfully Imported!"
+echo "==========================================="
+echo "TemplateFile: ${TemplateFile}"
+echo "StackName: ${Environment}-${ResourcePrefix}-${stack}"
+echo "PipelineName: ${pipeLine}"
+echo "CodeBuildName: ${codeBuild}"
+echo "ArtifactBucketName: ${ArtifactBucket}"
+echo "GitHubConnectionName: ${connection}"
+echo "GitHubProviderType: ${GitHubProviderType}"
+echo "GitHubRepo: ${GitrepositoryName}"
+echo "GitHubBranch: ${GitBranch}"
+echo "GitHubOwner: ${GitHubOwner}"
+echo "Region: ${REGION}"
+echo "Tags: ${Tags}"
+echo "==========================================="
 
 # Deploy CloudFormation stack using AWS SAM
 echo "Deploying AWS CloudFormation Pipeline..."
 
 sam deploy \
     -t "${TemplateFile}" \
-    --stack-name "${Environment}-${ResourcePrefix}-$" \
+    --stack-name "${Environment}-${ResourcePrefix}" \
     --region "${REGION}" \
     --no-fail-on-empty-changeset \
     --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
@@ -32,6 +46,5 @@ sam deploy \
         "GitHubOwner=${GitHubOwner}" \
         "Region=${REGION}" \
         "Tags=${Tags}" \
- 
-   
+
 echo "All deployments were successful!"
